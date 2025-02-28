acebe365dbdda71c84da22bbe8115d24d6ca0323
Maria <masha-kem133@mail.ru>
2025-02-28 18:12:47 +0100

    Updates

--- assets/requireAuth-CurkGT4u.js
+++
@@ -1,1 +0,0 @@
-import{ag as o,u as a,j as r,aV as u}from"./index-BIGqcniS.js";const c=({children:t})=>{const s=o();return a(e=>e.user.user.token)?t:r.jsx(u,{to:"/sign-in",state:{from:s}})};export{c as RequireAuth};
---
+++ assets/requireAuth-DcQSw37U.js
@@ -0,0 +1,1 @@
+import{ag as o,u as a,j as r,aV as u}from"./index-CtUhi32C.js";const c=({children:t})=>{const s=o();return a(e=>e.user.user.token)?t:r.jsx(u,{to:"/sign-in",state:{from:s}})};export{c as RequireAuth};
